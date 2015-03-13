600e44244b51f748ccf2795bfb9c40b00dab392d
Diego Haz <hazdiego@gmail.com>
2015-03-13 16:51:58 -0300

    Define 'close a request' service

--- cloud/main.js
+++ cloud/main.js
@@ -32,47 +32,31 @@ Parse.Cloud.define('getRequests', function(request, response) {
 });
 
 /**
- * Respond a request
+ * Get user's requests
  *
- * @param string requestId
- * @param bool optional hasItem
+ * @param int optional limit
+ * @param int optional page
  *
- * @response void
+ * @todo Archive old requests
+ *
+ * @response array List of request objects
  */
-Parse.Cloud.define('respond', function(request, response) {
+Parse.Cloud.define('getUserRequests', function(request, response) {
   Parse.Cloud.useMasterKey();
 
   // Params
-  var helper = request.user;
-  var hasItem = request.params.hasItem || true;
+  var limit = request.params.limit || 30;
+  var page = request.params.page || 1;
 
-  // Get request
+  // query
   var query = new Parse.Query('Request');
+  query.equalTo('author', request.user);
+  query.include(['author', 'item']);
+  query.ascending('createdAt');
+  query.limit(limit);
+  query.skip((page - 1) * limit);
 
-  query.get(request.params.requestId).then(function(req) {
-    if (hasItem && !req.get('open')) {
-      // Add item to user's inventory
-      helper.addUnique('inventory', req.get('item'));
-      helper.save();
-
-      // Change request's state
-      req.set('open', true);
-      req.set('helper', helper);
-      return req.save();
-    } else if (!hasItem) {
-      // Remove item from user's inventory and add to user's hasNot list
-      helper.remove('inventory', req.get('item'));
-      helper.addUnique('hasNot', req.get('item'));
-
-      return helper.save();
-    } else {
-      return Parse.Promise.error();
-    }
-  }).then(function() {
-    response.success();
-  }, function() {
-    response.error();
-  });
+  query.find().then(response.success, response.error);
 });
 
 /**
@@ -109,3 +93,83 @@ Parse.Cloud.define('getOpenRequests', function(request, response) {
 
   query.find().then(response.success, response.error);
 });
+
+/**
+ * Respond a request
+ *
+ * @param string requestId
+ * @param bool optional hasItem
+ *
+ * @response void
+ */
+Parse.Cloud.define('respond', function(request, response) {
+  Parse.Cloud.useMasterKey();
+
+  // Params
+  var helper = request.user;
+  var requestId = request.params.requestId;
+  var hasItem = request.params.hasItem || true;
+
+  // Get request
+  var query = new Parse.Query('Request');
+
+  query.get(requestId).then(function(req) {
+    if (hasItem && !req.get('open')) {
+      // Add item to user's inventory
+      helper.addUnique('inventory', req.get('item'));
+      helper.save();
+
+      // Change request's state
+      req.set('open', true);
+      req.set('helper', helper);
+      return req.save();
+    } else if (!hasItem) {
+      // Remove item from user's inventory and add to user's hasNot list
+      helper.remove('inventory', req.get('item'));
+      helper.addUnique('hasNot', req.get('item'));
+
+      return helper.save();
+    } else {
+      return Parse.Promise.error();
+    }
+  }).then(response.success, response.error);
+});
+
+/**
+ * Close a request
+ *
+ * @param string requestId
+ * @param bool optional successful
+ *
+ * @response void
+ */
+Parse.Cloud.define('close', function(request, response) {
+  Parse.Cloud.useMasterKey();
+
+  // Params
+  var requestId = request.params.requestId;
+  var successful = request.params.successful || true;
+
+  // Query request
+  var query = new Parse.Query('Request');
+  query.include('helper');
+
+  query.get(requestId).then(function(req) {
+    var helper = req.get('helper');
+
+    if (successful) {
+      // Increment helper's requests limit
+      helper.increment('requestsLimit');
+      helper.save();
+
+      // Close request
+      req.set('open', false);
+      return req.save();
+    } else {
+      // Unassign helper and keep request open
+      req.set('helper', null);
+      return req.save();
+    }
+  }).then(response.success, response.error);
+});
+
