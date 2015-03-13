8e9ece3ff4d3b6f83ecd2eff733acda1d77b618b
Diego Haz <hazdiego@gmail.com>
2015-03-13 15:03:50 -0300

    Define requests service

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,30 @@
+var _ = require('underscore');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+/**
+ * Get requests
+ *
+ * @param int limit
+ * @param int page
+ *
+ * @todo order requests by user's genre, course and inventory.
+ *
+ * @response array list of request objects
+ */
+Parse.Cloud.define('requests', function(request, response) {
+  // Params
+  var genre = request.user.get('genre');
+  var course = request.user.get('course');
+  var location = request.user.get('location');
+  var inventory = request.user.get('inventory');
+  var limit = request.params.limit || 30;
+  var page = request.params.page || 1;
+
+  // Query
+  var query = new Parse.Query('Request');
+  query.equalTo('open', false);
+  query.include(['author', 'item']);
+  query.limit(limit);
+  query.skip((page - 1) * limit);
+
+  query.near('author.location').then(response.success, response.error);
 });
